bedf6872fb5c1db7b04b92ea4860030d9270c9b9
IAmAlexPuga <pugapower@gmail.com>
2021-08-19 19:02:14 -0500

    chapter 3

---
+++ Ch1/AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Having trouble? Send us a message.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string msg)
+        {
+            // TODO: Send message
+            ViewBag.Message = "Thanks we go your message.";
+
+            return View();
+        }
+
+
+
+        public ActionResult Foo()
+        {
+            return View("About");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "23adf43";
+
+            if(letterCase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+
+            //return new HttpStatusCodeResult(403);
+            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
