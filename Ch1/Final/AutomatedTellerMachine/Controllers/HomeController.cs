f4ac9e0f151c6533aaf070e45612706073634d74
IAmAlexPuga <pugapower@gmail.com>
2021-08-19 19:46:43 -0500

    learned about razor syntax

--- Ch1/Final/AutomatedTellerMachine/Controllers/HomeController.cs
+++ Ch1/Final/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -8,56 +8,54 @@ namespace AutomatedTellerMachine.Controllers
 {
     public class HomeController : Controller
     {
+        // GET /home/index
+        [MyLoggingFilter]
         public ActionResult Index()
         {
+            // throw new StackOverflowException();
             return View();
         }
 
-        // sets the name of the name 
-        [ActionName("about-this-atm")]
+        // GET /home/about
+        // [ActionName("about-this-atm")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            // if use ActionName, must tell to still look for the "About" view instead of the "about-this-atm"
             return View("About");
         }
-
+                
         public ActionResult Contact()
         {
-            ViewBag.Message = "Having trouble? Send us a message.";
+            ViewBag.TheMessage = "Having trouble? Send us a message.";            
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult Contact(string msg)
+        public ActionResult Contact(string message)
         {
-            // TODO: Send message
-            ViewBag.Message = "Thanks we go your message.";
+            // TODO: send the message to HQ
+            ViewBag.TheMessage = "Thanks, we got your message!";
 
             return View();
         }
 
-
-
         public ActionResult Foo()
-        {
+        {            
             return View("About");
         }
 
         public ActionResult Serial(string letterCase)
         {
-            var serial = "23adf43";
-
-            if(letterCase == "lower")
+            var serial = "ASPNETMVC5ATM1";
+            if (letterCase == "lower")
             {
                 return Content(serial.ToLower());
             }
-
-            //return new HttpStatusCodeResult(403);
-            return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+            // return new HttpStatusCodeResult(403);
+            // return Json(new { name = "serial number", value = "serial" }, JsonRequestBehavior.AllowGet);
+            return RedirectToAction("Index");
         }
-
     }
 }
